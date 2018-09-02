a58b2439d7f2c83459339914991d41b411d46c5b
Camm66 <Cam.morales93@gmail.com>
2018-09-01 23:01:11 -0500

    Neural Network Class Skeleton code

---
+++ NeuralNetwork.py
@@ -0,0 +1,11 @@
+#neural network class definition
+class neuralNetwork:
+
+    #initialise the network
+    def __init__():
+
+    #train the network
+    def train():
+
+    #query the network
+    def query():
